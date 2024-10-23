d00a8b6879191d8b093d05ac9cfb8fb69d996d74
Osama-Elwan <osama.elwan01@gmail.com>
2024-10-23 21:03:57 +0300

    end of emphasize blades

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,43 @@
-<div>
+<h1>
 
-    Hello Im a blade template
-</div>
-@isset($name)
+    {{-- Hello Im a blade template --}}
+    The list of tasks
+</h1>
+{{-- @isset($name)
 <div>
 
     The name is : {{ $name }}
 </div>
-@endisset
+@endisset --}}
+
+
+{{-- <div> --}}
+    {{-- @dd($tasks) --}}
+    {{-- @if (count($tasks)) --}}
+        {{-- <div>there are tasks</div> --}}
+        {{-- @foreach ($tasks as $task) --}}
+            {{-- <div>{{ $task->title }}</div> --}}
+        {{-- @endforeach --}}
+        {{-- @else --}}
+        {{-- <div>there are no tasks</div> --}}
+    {{-- @endif --}}
+{{-- </div> --}}
+
+<div>
+
+
+        {{-- <div>there are tasks</div> --}}
+        @forelse ( $tasks as $task )
+        <div>
+
+            <a href="{{ route('tasks.show',['id'=>$task->id]) }}">{{ $task->title }}</a>
+        </div>
+        {{-- <div>{{ $task->title }}</div> --}}
+        @empty
+
+        <div>there are no tasks</div>
+        @endforelse ($tasks as $task)
+
+
+
+</div>
